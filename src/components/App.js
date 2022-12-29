25d29f3ec24d28d594b9036cf7b5a5e5f453eacc
Aniket Adavkar <aniketadavkar1@gmail.com>
2022-12-29 15:44:18 +0530

    code editor

---
+++ src/components/App.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import Editor from './Editor'
+import useLocalStorage from '../hooks/useLocalStorage'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+function App() {
+  const [html, setHtml] = useLocalStorage('html', '')
+  const [css, setCss] = useLocalStorage('css', '')
+  const [js, setJs] = useLocalStorage('js', '')
+  const [srcDoc, setSrcDoc] = useState('')
+  const headStyle = 
+  {
+    padding: '20px',
+    width: '100%',
+    fontWeight: 'bold',
+    fontSize: '30px',
+    fontVariant: 'small-caps'
+  }
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>
+      `)
+    }, 250)
+
+    return () => clearTimeout(timeout)
+  }, [html, css, js])
+
+  return (
+    <>
+
+    <div style={headStyle}> &#60; CodeBite /  &#62;<FontAwesomeIcon icon="fa-thin fa-cookie-bite" /></div>
+      <div className="pane top-pane">
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="javascript"
+          displayName="JS"
+          value={js}
+          onChange={setJs}
+        />
+      </div>
+      <div className="pane">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
+  )
+}
+
+export default App;
